2cc9779b8eeabb0f1a948ee0c94b77a1c0cb9540
Pablo-ReyesGit <125313745+Pablo-ReyesGit@users.noreply.github.com>
2025-04-07 23:01:52 -0600

    cambio a la gramatica

---
+++ src/test/java/Simbolo.java
@@ -0,0 +1,24 @@
+
+public class Simbolo {
+    private String nombre;
+    private String tipo;
+    private String valor;
+
+    public Simbolo(String nombre, String tipo, String valor) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.valor = valor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+}
